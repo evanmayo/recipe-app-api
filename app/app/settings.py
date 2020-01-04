67437201a1d7a4ce56476c3989ce4395492d1735
evanmayo <evanmleong@gmail.com>
2020-01-03 17:22:09 -0800

    reverted to previous settings build

--- app/app/settings.py
+++ app/app/settings.py
@@ -126,9 +126,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR,'vol/web/static')
-
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR,'vol/web/media')
+
+MEDIA_ROOT = '/vol/web/media'
+STATIC_ROOT = '/vol/web/static'
 
 AUTH_USER_MODEL = 'core.User'
